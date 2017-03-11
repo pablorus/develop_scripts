fe38e09a763e0d64dea0a0cf9822d3fbea84e771
pablo <paborus@mail.ru>
2017-03-11 11:53:05 +0300

    + AddComment Sublime Plugin

---
+++ sublime_text/add_comment.py
@@ -0,0 +1,17 @@
+# Плагин автокомментирования
+
+import datetime, getpass
+import sublime, sublime_plugin
+
+
+class AddDateCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.run_command("insert_snippet", { "contents": "%s" %  datetime.date.today().strftime("%d %B %Y (%A)") } )
+
+class AddTimeCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.run_command("insert_snippet", { "contents": "%s" %  datetime.datetime.now().strftime("%H:%M") } )
+
+class AddCheckedCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.run_command("insert_snippet", { "contents": "\n# %s checked. P.Rusanov\n# Отлично!" %  datetime.datetime.now().strftime("%Y.%m.%d %H:%M:%S") } )
